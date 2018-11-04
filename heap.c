2245012d4cc68f691b36245e6edd751337f1b45c
Agustin More <agustin427more@gmail.com>
2018-11-03 21:46:12 -0300

    heap_encolar

--- heap.c
+++ heap.c
@@ -5,12 +5,15 @@
 #include <stdio.h>
 
 #define TAM_INICIAL 100
+#define FACTOR_ENC 0.8
+#define FACTOR_CRECIMIENTO 3
 #define PADRE(hijo) (size_t)((hijo - 1) / 2)
 #define HIJO_IZQ(padre) (size_t)((padre * 2) + 1)
 #define HIJO_DER(padre) (size_t)((padre * 2) + 2)
 
-void swap(void * datos[], size_t i, size_t j);
+void swap(void * elemento_1, void * elemento_2);
 void upheap(heap_t * heap, size_t i);
+bool heap_redimensionar(heap_t * heap, size_t nueva_capacidad);
 
 struct heap{
 	void ** datos;
@@ -42,11 +45,11 @@ heap_t *heap_crear(cmp_func_t cmp) {
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp) {
 	
-	heap_t* heap=heap_crear(cmp);
+	heap_t* heap = heap_crear(cmp);
 	
-	if(!heap)return NULL;
+	if(!heap) return NULL;
 	
-	for(int i=0;i<n;i++) heap->datos[i]=arreglo[i];
+	for(int i = 0; i < n; i++) heap->datos[i] = arreglo[i];
 	
 	return heap;
 }
@@ -78,9 +81,20 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
 	free(heap);
 }
 
-
 bool heap_encolar(heap_t *heap, void *elem) {
+	size_t cant = heap->cant;
+	size_t tam = heap->tam;
+	cmp_func_t cmp = heap->cmp;
 
+	if(cant > tam * FACTOR_ENC)
+		if(!heap_redimensionar(heap, tam * FACTOR_CRECIMIENTO)) return false;
+
+	heap->datos[cant] = elem;
+	upheap(heap, cant);	
+	
+	heap->cant++;
+
+	return true;
 }
 
 void *heap_desencolar(heap_t *heap) {
@@ -96,10 +110,10 @@ void pruebas_heap_alumno(void) {
  *                FUN. AUXILIARES
  *****************************************************/
 
-void swap(void * datos[], size_t i, size_t j) {
-	void * aux = datos[i];
-	datos[i] = datos[j];
-	datos[j] = datos[i];
+void swap(void * elemento_1, void * elemento_2) {
+	void * aux = elemento_1;
+	elemento_1 = elemento_2;
+	elemento_2 = aux;
 }
 
 void upheap(heap_t * heap, size_t i) {
@@ -109,7 +123,15 @@ void upheap(heap_t * heap, size_t i) {
 	void ** datos = heap->datos;
 
 	if(cmp(datos[i], datos[PADRE(i)]) > 0) {
-		swap(datos, i, PADRE(i));
+		swap(datos[i], datos[PADRE(i)]);
 		upheap(heap, PADRE(i));
 	}
+}
+
+bool heap_redimensionar(heap_t * heap, size_t nueva_capacidad) {
+	void ** nuevo_arr = realloc(heap->datos, nueva_capacidad * sizeof(void *));
+	if(!nuevo_arr) return false;
+	heap->datos = nuevo_arr;
+	heap->tam = nueva_capacidad;
+	return true;
 }
